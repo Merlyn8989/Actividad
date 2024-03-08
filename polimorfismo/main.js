3eef854aca08fd7487144a4dbbed10683d680d82
Merlyn8989 <140095326+Merlyn8989@users.noreply.github.com>
2024-03-07 22:05:32 -0600

    Add files via upload
    Tarea

---
+++ polimorfismo/main.js
@@ -0,0 +1,27 @@
+class Complejos{
+    constructor(nombre, tipo, comportamiento){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.comportamiento = comportamiento;
+    }
+
+    diagnostico(){
+        return `la persona ${this.nombre} tiene el complejo de ${this.tipo} por lo que se comporta ${this.comportamiento}`
+    }
+    
+}
+
+class Inferioridad extends Complejos{
+    diagnostico(){
+        return`la persona ${this.nombre} tiene el complejo de ${this.tipo} por lo que se comporta ${this.comportamiento} y se debe a un rechazo o acoso sufrido al paciente`
+    }
+}
+
+class Superioridad extends Complejos{
+    diagnostico(){
+        return`la persona ${this.nombre} tiene el complejo de ${this.tipo} por lo que se comporta ${this.comportamiento} y se debe a elogios constantes o fomentar su ego, también a miedos internos`
+    }
+}
+
+const paciente1 = new Inferioridad("Samanta", "Inferioridad", "Sensación de ser menos capaz o valioso que los demás");
+const paciente2 = new Superioridad("Francisco", "Superioridad", "Excesiva arrogancia");
